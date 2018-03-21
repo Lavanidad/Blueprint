0289dfcca9cec4a32b8e2b652fe07a877f2ccad8
Lavanidad <f754458151@gmail.com>
2018-03-21 17:55:08 +0800

    test

---
+++ app/src/main/java/com/deepspring/blueprint/activity/MainActivity.java
@@ -0,0 +1,103 @@
+package com.deepspring.blueprint.activity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+import android.widget.Toast;
+
+import com.ashokvarma.bottomnavigation.BottomNavigationBar;
+import com.ashokvarma.bottomnavigation.BottomNavigationItem;
+import com.deepspring.blueprint.R;
+import com.deepspring.blueprint.base.BaseActivity;
+import com.deepspring.blueprint.fragment.DailyFragment;
+import com.deepspring.blueprint.fragment.MapFragment;
+import com.deepspring.blueprint.fragment.UserFragment;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends BaseActivity implements BottomNavigationBar.OnTabSelectedListener {
+
+    @BindView(R.id.activity_container)
+    FrameLayout mFragmentContainer;
+    @BindView(R.id.bottom_navigation_bar)
+    BottomNavigationBar mBottomNavigationBar;
+    private DailyFragment mDailyFragment;
+    private MapFragment mMapFragment;
+    private UserFragment mUserFragment;
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected void initViews(){
+        super.initViews();
+        InitNavigationBar();
+        ButterKnife.bind(this);
+    }
+
+    private void InitNavigationBar() {
+        BottomNavigationBar bottomNavigationBar = (BottomNavigationBar) findViewById(R.id.bottom_navigation_bar);
+        bottomNavigationBar
+                .addItem(new BottomNavigationItem(R.mipmap.daily_32dp, "发现").setActiveColorResource(R.color.bottom_bg_color))
+                .addItem(new BottomNavigationItem(R.mipmap.run_32dp, "运动").setActiveColorResource(R.color.colorPrimary))
+                .addItem(new BottomNavigationItem(R.mipmap.user_32dp, "我的").setActiveColorResource(R.color.colorAccent))
+                .setFirstSelectedPosition(1)
+                .setMode(BottomNavigationBar.MODE_SHIFTING)
+                .setBackgroundStyle(BottomNavigationBar.BACKGROUND_STYLE_STATIC)
+                .initialise();
+    }
+
+    @Override
+    public void onTabSelected(int position) {
+        Log.d("onTabSelected", "onTabSelected: " + position);
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction transaction = fm.beginTransaction();
+        switch (position) {
+            case 0:
+                if (mDailyFragment == null) {
+                    mDailyFragment = DailyFragment.newInstance();
+                }
+                transaction.replace(R.id.activity_container, mDailyFragment);
+                break;
+            case 1:
+                if (mMapFragment == null) {
+                    mMapFragment = MapFragment.newInstance();
+                }
+                transaction.replace(R.id.activity_container, mMapFragment);
+                break;
+            case 2:
+                if (mUserFragment == null) {
+                    mUserFragment = UserFragment.newInstance();
+                }
+                transaction.replace(R.id.activity_container, mUserFragment);
+                break;
+            default:
+                break;
+        }
+        // 事务提交
+        transaction.commit();
+    }
+
+    @Override
+    public void onTabUnselected(int position) {
+        Log.d("onTabUnselected", "onTabUnselected: " + position);
+    }
+
+    @Override
+    public void onTabReselected(int position) {
+        Log.d("onTabReselected", "onTabReselected: " + position);
+    }
+
+}
---
+++ app/src/main/java/com/deepspring/blueprint/base/BaseActivity.java
@@ -0,0 +1,116 @@
+package com.deepspring.blueprint.base;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+import android.widget.Toast;
+
+import com.ashokvarma.bottomnavigation.BottomNavigationBar;
+import com.ashokvarma.bottomnavigation.BottomNavigationItem;
+import com.deepspring.blueprint.R;
+import com.deepspring.blueprint.fragment.DailyFragment;
+import com.deepspring.blueprint.fragment.MapFragment;
+import com.deepspring.blueprint.fragment.UserFragment;
+
+
+public abstract class BaseActivity extends AppCompatActivity {
+    protected DrawerLayout mRootView;
+    private Toast mToast;
+    protected FrameLayout flActivityContainer;
+
+    private NavigationView.OnNavigationItemSelectedListener mListener = new NavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.nav_github:
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/Lavanidad")));
+                    break;
+                case R.id.nav_wechat:
+                    toastShort("wechat clicked");
+                    break;
+                case R.id.nav_about:
+                   toastShort("about clicked");
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drawer);
+        initDatas();
+        initViews();
+    }
+
+    @LayoutRes
+    protected abstract int getLayoutId();
+
+    /**
+     * 初始化数据
+     */
+    protected void initDatas() {
+
+    }
+
+    /**
+     * 初始化view,在 initDatas 后调用
+     */
+    protected void initViews() {
+        flActivityContainer = findViewById(R.id.activity_container);
+        flActivityContainer.addView(LayoutInflater.from(this).inflate(getLayoutId(), flActivityContainer, false));
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(mListener);
+        mRootView = findViewById(R.id.root_layout);
+    }
+
+
+
+    /**
+     * 发出一个短Toast
+     *
+     * @param text 内容
+     */
+    public void toastShort(String text) {
+        toast(text, Toast.LENGTH_SHORT);
+    }
+
+    /**
+     * 发出一个长toast提醒
+     *
+     * @param text 内容
+     */
+    public void toastLong(String text) {
+        toast(text, Toast.LENGTH_LONG);
+    }
+
+    private void toast(final String text, final int duration) {
+        if (!TextUtils.isEmpty(text)) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    if (mToast == null) {
+                        mToast = Toast.makeText(getApplicationContext(), text, duration);
+                    } else {
+                        mToast.setText(text);
+                        mToast.setDuration(duration);
+                    }
+                    mToast.show();
+                }
+            });
+        }
+    }
+}
