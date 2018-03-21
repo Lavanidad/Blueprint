fcbda2990285bd660e082a3f59cc9926df8a48a6
Lavanidad <f754458151@gmail.com>
2018-03-21 23:09:27 +0800

    BaseAc、Fra

--- app/src/main/java/com/deepspring/blueprint/activity/MainActivity.java
+++ app/src/main/java/com/deepspring/blueprint/activity/MainActivity.java
@@ -1,18 +1,10 @@
 package com.deepspring.blueprint.activity;
 
-import android.content.Intent;
-import android.net.Uri;
-import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.NavigationView;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.MenuItem;
 import android.widget.FrameLayout;
-import android.widget.Toast;
+
 
 import com.ashokvarma.bottomnavigation.BottomNavigationBar;
 import com.ashokvarma.bottomnavigation.BottomNavigationItem;
@@ -22,15 +14,19 @@ import com.deepspring.blueprint.fragment.DailyFragment;
 import com.deepspring.blueprint.fragment.MapFragment;
 import com.deepspring.blueprint.fragment.UserFragment;
 
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
+//todo-list 1、toolbar 2、fragment修改封装 3、框架细节调整完成 4、计步功能加入主页或者第一页
 public class MainActivity extends BaseActivity implements BottomNavigationBar.OnTabSelectedListener {
 
     @BindView(R.id.activity_container)
     FrameLayout mFragmentContainer;
     @BindView(R.id.bottom_navigation_bar)
     BottomNavigationBar mBottomNavigationBar;
+
+    private static int LAST_SELECTED_POSITION = 0;
     private DailyFragment mDailyFragment;
     private MapFragment mMapFragment;
     private UserFragment mUserFragment;
@@ -39,8 +35,6 @@ public class MainActivity extends BaseActivity implements BottomNavigationBar.On
     protected int getLayoutId() {
         return R.layout.activity_main;
     }
-
-    //testtest111111
 
     @Override
     protected void initViews(){
@@ -52,54 +46,67 @@ public class MainActivity extends BaseActivity implements BottomNavigationBar.On
     private void InitNavigationBar() {
         BottomNavigationBar bottomNavigationBar = (BottomNavigationBar) findViewById(R.id.bottom_navigation_bar);
         bottomNavigationBar
-                .addItem(new BottomNavigationItem(R.mipmap.daily_32dp, "发现").setActiveColorResource(R.color.bottom_bg_color))
+                .addItem(new BottomNavigationItem(R.mipmap.daily_32dp, "发现").setActiveColorResource(R.color.colorAccent))
                 .addItem(new BottomNavigationItem(R.mipmap.run_32dp, "运动").setActiveColorResource(R.color.colorPrimary))
                 .addItem(new BottomNavigationItem(R.mipmap.user_32dp, "我的").setActiveColorResource(R.color.colorAccent))
-                .setFirstSelectedPosition(1)
+                .setFirstSelectedPosition(LAST_SELECTED_POSITION)
+                .setTabSelectedListener(this)
                 .setMode(BottomNavigationBar.MODE_SHIFTING)
                 .setBackgroundStyle(BottomNavigationBar.BACKGROUND_STYLE_STATIC)
-                .initialise();
+                .initialise();//end
+        setDefaultFragment();//设置默认
     }
 
-    @Override
-    public void onTabSelected(int position) {
-        Log.d("onTabSelected", "onTabSelected: " + position);
+    private void setDefaultFragment() {
         FragmentManager fm = getSupportFragmentManager();
         FragmentTransaction transaction = fm.beginTransaction();
-        switch (position) {
-            case 0:
-                if (mDailyFragment == null) {
-                    mDailyFragment = DailyFragment.newInstance();
-                }
-                transaction.replace(R.id.activity_container, mDailyFragment);
-                break;
-            case 1:
-                if (mMapFragment == null) {
-                    mMapFragment = MapFragment.newInstance();
-                }
-                transaction.replace(R.id.activity_container, mMapFragment);
-                break;
-            case 2:
-                if (mUserFragment == null) {
-                    mUserFragment = UserFragment.newInstance();
-                }
-                transaction.replace(R.id.activity_container, mUserFragment);
-                break;
-            default:
-                break;
-        }
-        // 事务提交
+        transaction.replace(R.id.main_container, DailyFragment.newInstance("发现"));
         transaction.commit();
     }
 
     @Override
-    public void onTabUnselected(int position) {
-        Log.d("onTabUnselected", "onTabUnselected: " + position);
+    public void onTabSelected(int position) {
+       FragmentManager fm = this.getSupportFragmentManager();
+        FragmentTransaction transaction = fm.beginTransaction();
+        switch (position) {
+            case 0:
+                if (mDailyFragment == null) {
+                    mDailyFragment = DailyFragment.newInstance("发现");
+                }
+                transaction.replace(R.id.main_container, mDailyFragment);
+                break;
+            case 1:
+                if (mMapFragment == null) {
+                    mMapFragment = MapFragment.newInstance("运动");
+                }
+                transaction.replace(R.id.main_container, mMapFragment);
+                break;
+            case 2:
+                if (mUserFragment == null) {
+                    mUserFragment = UserFragment.newInstance("我的");
+                }
+                transaction.replace(R.id.main_container, mUserFragment);
+                break;
+            default:
+                break;
+        }
+        transaction.commit();// 事务提交
     }
 
+    /**
+     * 设置未选中Fragment 事务
+     */
+    @Override
+    public void onTabUnselected(int position) {
+    }
+
+    /**
+     * 设置释放Fragment 事务
+     */
     @Override
     public void onTabReselected(int position) {
         Log.d("onTabReselected", "onTabReselected: " + position);
     }
+
 
 }
