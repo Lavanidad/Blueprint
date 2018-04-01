5c51867b91a5cbea3619a9e52118105b9215df36
Lavanidad <f754458151@gmail.com>
2018-04-01 11:35:43 +0800

    map layout

--- app/src/main/java/com/deepspring/blueprint/fragment/Map/RunFragment.java
+++ app/src/main/java/com/deepspring/blueprint/fragment/Map/RunFragment.java
@@ -1,35 +1,42 @@
 package com.deepspring.blueprint.fragment.Map;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.deepspring.blueprint.R;
+import com.deepspring.blueprint.activity.RunActivity;
+import com.github.edsergeev.TextFloatingActionButton;
 
 
 public class RunFragment extends Fragment {
 
+    private TextFloatingActionButton mButton;
 
     public RunFragment() {
         // Required empty public constructor
     }
 
-
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-    }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_run, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_run,container,false);
+        mButton = rootView.findViewById(R.id.run_fab);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), RunActivity.class);
+                startActivity(intent);
+            }
+        });
+        return rootView;
     }
 
 
