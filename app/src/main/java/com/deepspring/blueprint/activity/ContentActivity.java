d207e7aaa070777fd8da6f95a4a7f2c3c31d6794
Lavanidad <f754458151@gmail.com>
2018-05-04 15:00:23 +0800

    news --test3

--- app/src/main/java/com/deepspring/blueprint/activity/ContentActivity.java
+++ app/src/main/java/com/deepspring/blueprint/activity/ContentActivity.java
@@ -2,12 +2,15 @@ package com.deepspring.blueprint.activity;
 
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 import com.deepspring.blueprint.R;
+
+import java.lang.reflect.Field;
 
 
 public class ContentActivity extends AppCompatActivity {
@@ -19,6 +22,14 @@ public class ContentActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.N){
+            try {
+                Class decorViewClazz = Class.forName("com.android.internal.policy.DecorView");
+                Field field = decorViewClazz.getDeclaredField("mSemiTransparentStatusBarColor");
+                field.setAccessible(true);
+                field.setInt(getWindow().getDecorView(), getColor(R.color.diy_red2));
+            } catch (Exception e) {}
+        }
         setContentView(R.layout.activity_content);
         webView = findViewById(R.id.web_view);
         webView.getSettings().setJavaScriptEnabled(true);
