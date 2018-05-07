494c0f2d272266ec8712e6c13baa3e29efc293ad
Lavanidad <f754458151@gmail.com>
2018-05-07 17:24:11 +0800

    login -test3

--- app/src/main/java/com/deepspring/blueprint/activity/ShowRecordActivity.java
+++ app/src/main/java/com/deepspring/blueprint/activity/ShowRecordActivity.java
@@ -9,6 +9,7 @@ import android.support.v7.app.ActionBar;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.amap.api.maps.AMap;
 import com.amap.api.maps.CameraUpdateFactory;
